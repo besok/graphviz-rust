besok/graphviz-rust

commit c3296f60fea6934b0d9a94ba78b5fb523907c214

use space instead of comma as separator of gattrs to make the output valid DOT syntax

src/printer.rs
@@ -290,7 +290,7 @@ impl DotPrinter for Vec<Attribute> {
             ctx.indent_shrink();
             r
         } else {
-            format!("[{}]", attrs.join(","))
+            format!("[{}]", attrs.join(" "))
         }
     }
 }

commit 0dad3834968a5ad759efeeaddd9f0eb248f06625

Fix tests
Broken by c3296f6

src/lib.rs
@@ -237,7 +237,7 @@ mod tests {
             ),
             edge!(node_id!("a1") => node_id!(esc "a2"))
         );
-        let graph_str = "graph id {\n  nod\n  subgraph sb {\n    a -- subgraph  {n[color=black,shape=egg]}\n  }\n  a1 -- \"a2\"\n}";
+        let graph_str = "graph id {\n  nod\n  subgraph sb {\n    a -- subgraph  {n[color=black shape=egg]}\n  }\n  a1 -- \"a2\"\n}";
 
         let mut ctx = PrinterContext::default();
         assert_eq!(graph_str, g.print(&mut ctx));

src/printer.rs
@@ -497,7 +497,7 @@ mod tests {
     fn graph_attr_test() {
         let mut ctx = PrinterContext::default();
         let n_attr = GraphAttributes::Node(vec![attr!("a", 2), attr!("b", 3)]);
-        assert_eq!(n_attr.print(&mut ctx), "node[a=2,b=3]".to_string());
+        assert_eq!(n_attr.print(&mut ctx), "node[a=2 b=3]".to_string());
     }
 
     #[test]